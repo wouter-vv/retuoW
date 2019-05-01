51f9e583f1f726ac6e83c10d0b916e0899375680
Wouter Vande Velde <wouter.vandevelde@student.odisee.be>
2019-05-01 15:31:31 +0200

    add all

---
+++ Retuow/Retuow/Scripts/changingFonts.js
@@ -0,0 +1,9 @@
+﻿window.onload = function () {
+    var ball = document.getElementById('firstname')
+    ball.style.marginLeft = "25px";
+};
+
+var fontType = ["Ultra", "VT323", "ZCOOL KuaiLe", "Black And White Picture", "Tulpen One", "Faster One"];
+var num;
+num = Math.floor(Math.random() * 6);
+document.getElementById("firstname").style.fontFamily = fontType[num];
